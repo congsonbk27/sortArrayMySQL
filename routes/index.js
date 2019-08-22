425072edd4a529f7b81a222ed9d8afa6d2c54038
congsonbk27 <congsonbk27@gmail.com>
2019-08-22 23:20:46 +0700

    add to mysql

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "",
+  database: "sondb"
+});
+
+con.connect(function (err) {
+  if (err) {
+    res.render('error', {
+      title: 'error',
+      err: err
+    });
+    throw err;
+  }
+  console.log("Connected!");
+});
+
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // console.log(req.query.count);
-  var count = req.query.count;
-
   var inputString = req.query.inputString;
   console.log('inputString:', inputString);
+
   var arrayInput = inputString.split(",");
   console.log('arrayInput:', arrayInput);
+
+  var inputString_1 = arrayInput.join(" a ");
+  console.log('inputString_1:', inputString_1);
 
   var arrayNumber = arrayInput.map(function (item) {
     return parseInt(item, 10);
@@ -19,12 +42,32 @@ router.get('/', function (req, res, next) {
   var arraySorted = arrayNumber.sort(function (a, b) { return a - b });
   console.log('arrayNumber Sorted:', arrayNumber);
 
+  var stringOutput = arraySorted.join(" a ");
+  console.log('stringOutput:', stringOutput);
+
+  if (stringOutput != NaN) {
+    insertDatatoTable(inputString_1, stringOutput);
+  }
+
   res.render('index', {
     title: 'Express',
-    count: count,
     inputString: inputString,
     arraySorted: arraySorted
   });
 });
+
+function insertDatatoTable(inputString, stringOutput) {
+  // con.connect(function (err) {
+  // if (err) throw err;
+  console.log("Connected!");
+  var sql = 'INSERT INTO stringArray (stringInput, stringSorted) VALUES ("' + inputString + '","' + stringOutput + '")';
+  console.log('sql : ' + sql);
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("INSERT INTO stringArray OK");
+  });
+  // });
+}
+
 
 module.exports = router;
--- sql_db/b03_create_table_db.js
+++ sql_db/b03_create_table_db.js
@@ -6,23 +6,23 @@ var con = mysql.createConnection({
     password: "",
     database: "sondb"
 });
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-    });
-});
-
 // con.connect(function (err) {
 //     if (err) throw err;
 //     console.log("Connected!");
-//     var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+//     var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
 //     con.query(sql, function (err, result) {
 //         if (err) throw err;
-//         console.log("Table altered");
+//         console.log("Table created");
 //     });
 // });
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE stringArray (id INT AUTO_INCREMENT PRIMARY KEY, stringInput VARCHAR(255), stringSorted VARCHAR(255))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+  });
 
